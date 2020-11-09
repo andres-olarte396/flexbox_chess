eb228a283362ee80c8a50668531577d0fa27218b
Andrés Olarte <andres.olarte396@gmail.com>
2020-11-09 00:15:06 -0500

    initial valifdations and styles

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let data = {};
 let chess = $("#chess");
+let data = {};
+let equipments = ["white", "black"];
+let turn = 0;
 fetch("/chess/data/initial.json")
   .then((res) => res.json())
   .catch((error) => console.error("Error:", error))
@@ -8,7 +10,7 @@ fetch("/chess/data/initial.json")
       for (let i = 0; i < data.board.columns; i++) {
         let r = `<div id="${data.columns[i]}" class="column">`;
         for (let j = 0; j < data.board.rows; j++) {
-          r = `${r}<div id="${j + 1}${data.columns[i]}" row="${j + 1}" column="${data.columns[i]}" class="row ${data.board.classes[(i + j) % 2]}" ondrop="drop(event)" ondragover="allowDrop(event)"></div>`;
+          r = `${r}<div id="${j + 1}${data.columns[i]}" row="${j + 1}" column="${data.columns[i]}" class="cell ${data.board.classes[(i + j) % 2]}" ondrop="drop(event)" ondragover="allowDrop(event)"></div>`;
         }
         r = `${r}</div>`;
         chess.html(`${chess.html()}${r}`);
@@ -17,22 +19,38 @@ fetch("/chess/data/initial.json")
         for (let i = 0; i < member.initial_quantity; i++) {
           let cell = $(`#${member.initial_row}${member.initial_columns.split(',')[i]}`);
           if (cell)
-            cell.html(`<icon id="${member.name}.${member.color}.${i + 1}" class="${member.name} ${member.color}" draggable="true" ondragstart="drag(event)" >${member.symbol}</icon>`);
+            cell.html(`<icon id="${member.name}.${member.color}.${i + 1}" title="${member.name} ${member.color}" name="${member.name}" symbol="${member.symbol}" class="${member.color}" row="${cell.row}" column="${cell.column}" draggable="true" ondragstart="drag(event)" />`);
         }
       });
     }
   });
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+function drag(ev) {
+  let equipment = equipments[turn];
+  if (ev.target.id.includes(equipment)) {
+    ev.dataTransfer.setData("id", ev.target.id);
+  } else {
+    alert(`This is turn of equipment ${equipment}.`);
+  }
+}
+function drop(ev) {
+  ev.preventDefault();
+  let member = document.getElementById(ev.dataTransfer.getData("id"));
+  let equipment = equipments[turn];
+  if (member.equipment.includes(equipment)) return;
 
-  function allowDrop(ev) {
-    ev.preventDefault();
+  let target = ev.target.localName === "icon" ? ev.target.parentNode : ev.target;
+  if (target.children.length > 0) {
+    let child = target.children[0];
+    if (child.id.includes(equipment)) return;
+    target.removeChild(child);
   }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
+
+  target.appendChild(member);
+  changeTurn();
+}
+function changeTurn() {
+  turn = turn == 0 ? 1 : 0;
+}
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ let host = '127.0.0.1';
 let port = 5000;
 let server = http.createServer(function (req, res) {
   fs.readFile(`.${req.url == '/' ? "/index.html" : req.url}`, 'UTF-8', function (err, contents) {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/html' });
+      res.end(err);
+    }
     res.writeHead(200);
     res.end(contents);
   });
-
-  // res.writeHead(404, { 'Content-Type': 'text/html' });
-  // res.end('<h1>This page is not found</h1>');
 });
 server.listen(port, host, function () {
   console.log(`This server is runing on http://${host}:${port}`);
